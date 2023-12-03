54cf7efe4770e7a9108e496907b0a8fa07142ec8
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-12-03 14:08:10 +0300

    add try except

--- src/video.py
+++ src/video.py
@@ -10,17 +10,25 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, id_video):
-        video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=id_video
-                                               ).execute()
-        self.video_id = id_video
-        self.video_link = 'https://youtu.be/' + id_video
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=id_video
+                                                   ).execute()
+            self.video_id = id_video
+            self.link = 'https://youtu.be/' + id_video
+            self.title = video_response['items'][0]['snippet']['title']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.video_id = id_video
+            self.link = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
 
 
 
